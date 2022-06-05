8cb22bf6591b6578b5328f5dec0abe4cfbaa952e
Blaise Hala <blaisehala@gmail.com>
2022-06-05 22:24:57 +0300

    adds the app configurations in the settings

--- zebu/settings.py
+++ zebu/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'manuz.apps.ManuzConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'zebu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zebu',
+        'USER': 'moringa',
+        'PASSWORD': 'Access',
     }
 }
 
